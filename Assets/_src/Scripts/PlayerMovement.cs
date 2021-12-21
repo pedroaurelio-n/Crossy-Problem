b49b49bcf1273b1a2ca46c3f94ea6173c1a08a53
pedroaurelio-n <69462126+pedroaurelio-n@users.noreply.github.com>
2021-12-20 22:36:05 -0300

    Cena criada para teste de movimentação do jogador utilizando o AirConsole

---
+++ Assets/_src/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private void Move(string direction)
+    {
+        switch (direction)
+        {
+            case "forward":
+                var finalPosition = 
+                transform.position += Vector3.forward;
+                break;
+
+            case "back":
+                transform.position += -Vector3.forward;
+                break;
+
+            case "right":
+                transform.position += Vector3.right;
+                break;
+
+            case "left":
+                transform.position += -Vector3.right;
+                break;
+
+            default: 
+                Debug.LogError("Unknown direction received."); 
+                break;
+        }
+    }
+
+    private void OnEnable()
+    {
+        ControllerLogic.OnMovementMessage += Move;
+    }
+
+    private void OnDisable()
+    {
+        ControllerLogic.OnMovementMessage -= Move;
+    }
+}
